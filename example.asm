70a55d3deead5df8d4db057762308c12c226ed2b
Jacob G-W <jacoblevgw@gmail.com>
2021-08-18 12:04:36 -0400

    fix example.asm, now it works

--- example.asm
+++ example.asm
@@ -1,20 +1,24 @@
 BITS 64
-	org 0x200000
 
 header:
-	dw 0x00008a97 ; magic
-	dw textsize
-	dw 0 ; data size
-	dw 0 ; bss size
-	dw entry
-	0 ; debug info stuff
-	0 ; debug info stuff
-	dq entry ; fat header
+	db 0x00, 0x00, 0x8a, 0x97 ; magic
+	db 0x00, 0x00, 0x00, 0x0b ; text size (big endian)
+	dd 0 ; data size
+	dd 0 ; bss size
+	db 0x00, 0x00, 0x00, 8 + 1 + 6 ; symbol table size
+	db 0x00, 0x20, 0x00, 0x28 ; entry
+	dd 0 ; debug info stuff
+	dd 0 ; debug info stuff
+	db 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x28; fat header
 
 text:
-entry:
 	mov rbp, 8
 	push 0
 	push 0
 	syscall
-	textsize equ $ - text
+
+symtab:
+	; _main
+	db 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x28; symbol value (addr)
+	db 'T' | 0x80 ; symbol type (global text in this case)
+	db "_main", 0 ; symbol name
